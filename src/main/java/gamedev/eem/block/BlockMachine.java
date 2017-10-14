d8b7d99946a83a85d66fd00870e2be6796cd4420
Ben Klemp <isenfireldc@gmail.com>
2017-10-13 21:06:31 -0500

    Final commit

--- src/main/java/gamedev/eem/block/BlockMachine.java
+++ src/main/java/gamedev/eem/block/BlockMachine.java
@@ -61,7 +61,7 @@ public class BlockMachine extends BlockContainer {
 
             if (tileentity instanceof TileEntityMachine)
             {
-                playerIn.displayGUIChest((TileEntityMachine)tileentity); //TODO ERROR Error caused here
+                playerIn.openGui((TileEntityMachine)tileentity, 0, worldIn, pos.getX(), pos.getY(), pos.getZ()); //TODO ERROR Error caused here
                 playerIn.addStat(StatList.FURNACE_INTERACTION);
             }
 
